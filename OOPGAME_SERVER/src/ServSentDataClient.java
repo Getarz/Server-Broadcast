452439b3975bc4f73bf152e6f024fe4c7857a448
GuitarThanapol <GuitarThanapol@DESKTOP-T6F4JNH.local>
2019-04-27 05:58:44 +0700

    buf

--- OOPGAME_SERVER/src/ServSentDataClient.java
+++ OOPGAME_SERVER/src/ServSentDataClient.java
@@ -2,12 +2,14 @@
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Stack;
 
@@ -17,50 +19,67 @@ public class ServSentDataClient extends Thread {
 	public int card[] = new int[52];
 	public int dekCard[] = new int[52];
 	public int checkCard[] = new int[52];
+	public int pointCard[] = new int[52];
+	public int pointPlayer[] = new int[4];
+	public String imagePlayer[] = new String[3];
+	
 	public ServSentDataClient() {
-//		int j = 1;
-//		int c = 1;
-//		for (int i = 0; i < card.length; i++) {
-//			card[i] = "" + (j) + "-" + (c);
-//			System.out.println("card : " + card[i] + " " + i);
-//			if (c == 4) {
-//				c = 0;
-//				j++;
-//			}
-//			c++;
-//		}
-//		
-
-		for (int i=0 ; i<card.length ; i++){
-			card[i] = i+1;
+		for (int i = 0; i < card.length; i++) {
+			card[i] = i + 1;
 		}
-		for (int i=0 ; i<dekCard.length; i++){
-			int random = (int) (Math.random()*52);
-			if(i==0){
+		for (int i = 0; i < dekCard.length; i++) {
+			int random = (int) (Math.random() * 52);
+			if (i == 0) {
 				dekCard[i] = random;
 				stackk.push(dekCard[i]);
 				checkCard[random] = 1;
-			}
-			else {
-				if(checkCard[random]==0) {
+			} else {
+				if (checkCard[random] == 0) {
 					dekCard[i] = random;
 					stackk.push(dekCard[i]);
 					checkCard[random] = 1;
-				}
-				else {
+				} else {
 					i--;
 				}
 			}
 		}
 		for (int i = 0; i < dekCard.length; i++) {
-			if(i==0) {
-				System.out.print(dekCard[i]+"\t");
-			}
-			else {
-				System.out.print(dekCard[i]+"\t");
-				if(i%10==0)
+			if (i == 0) {
+				System.out.print(dekCard[i] + "\t");
+			} else {
+				System.out.print(dekCard[i] + "\t");
+				if (i % 10 == 0)
 					System.out.println();
 			}
+		}
+		int numcard2 = 1;
+		for (int i = 0; i < pointCard.length; i++) {
+			if (i >= 0 && i <= 9) {
+				pointCard[i] = numcard2;
+				numcard2++;
+			} else if (i >= 10 && i <= 12) {
+				pointCard[i] = 10;
+				numcard2 = 1;
+			} else if (i >= 13 && i <= 22) {
+				pointCard[i] = numcard2;
+				numcard2++;
+			} else if (i >= 23 && i <= 25) {
+				pointCard[i] = 10;
+				numcard2 = 1;
+			} else if (i >= 26 && i <= 35) {
+				pointCard[i] = numcard2;
+				numcard2++;
+			} else if (i >= 36 && i <= 38) {
+				pointCard[i] = 10;
+				numcard2 = 1;
+			} else if (i >= 39 && i <= 48) {
+				pointCard[i] = numcard2;
+				numcard2++;
+			} else if (i >= 49 && i <= 51) {
+				pointCard[i] = 10;
+				numcard2 = 1;
+			}
+			System.out.println(pointCard[i] + " " + i + " ");
 		}
 	}
 
@@ -85,6 +104,7 @@ public class ServSentDataClient extends Thread {
 							for (int j = 0; j < str.length; j++) {
 								if (str[0].equals(BroadcastServer.stack[j])) {
 									line = line + "-" + j;
+									imagePlayer[j] = str[1];
 								}
 							}
 							check++;
@@ -92,18 +112,39 @@ public class ServSentDataClient extends Thread {
 							sentData(line);
 						}
 						if (check == 3) {
+							int keep = 3;
 							for (int i = 0; i < 2; i++) {
-								for (int j = 0; j < BroadcastServer.stack.length-1; j++) {
-									
+								for (int j = 0; j < BroadcastServer.stack.length - 1; j++) {
+									int pop = (int) stackk.pop();
 									Socket sock = new Socket(BroadcastServer.stack[j], 50113);
 									PrintStream dat = new PrintStream(sock.getOutputStream());
-									line = BroadcastServer.stack[j]+"-"+stackk.pop()+"-"+j;
-									System.out.println(line+" "+BroadcastServer.stack[j]);
+									line = BroadcastServer.stack[j] + "-" + pop + "-" + j;
+									System.out.println(line + " " + BroadcastServer.stack[j]);
+									System.out.println("Point card : " + pointCard[pop] + " index " + pop);
+									pointPlayer[j] = (pointPlayer[j] + pointCard[pop]) % 10;
+									pop = (int) stackk.pop();
+									pointPlayer[3] = (pointPlayer[3] + pointCard[pop]) % 10;
 									dat.println(line);
 									dat.close();
 								}
 							}
-							check=0;
+							System.out.println("PLayer " + 0 + " : " + pointPlayer[0]);
+							System.out.println("PLayer " + 1 + " : " + pointPlayer[1]);
+							System.out.println("PLayer " + 2 + " : " + pointPlayer[2]);
+							System.out.println("Bot " + 3 + " : " + pointPlayer[3]);
+							
+							for (int i = 0; i < 3; i++) {
+								if (pointPlayer[3] < pointPlayer[i]) {
+									int s = Integer.parseInt(imagePlayer[i]);
+									sentData("win",BroadcastServer.stack[i],s);
+								}
+								else {
+									sentData("lost",BroadcastServer.stack[i],5);
+								}
+							}
+							System.out.println("Winner : "+pointPlayer[keep]);
+							
+							check = 0;
 						}
 						if (str.length == 4 && str[0].equals("chat")) {
 							sentData(line);
@@ -125,13 +166,11 @@ public class ServSentDataClient extends Thread {
 
 	public void sentData(String line) {
 		try {
-			System.out.println(line);
 			Socket socket1 = new Socket(BroadcastServer.stack[0], 50113);
 			PrintStream dataOut = new PrintStream(socket1.getOutputStream());
 			dataOut.println(line);
 			dataOut.close();
 
-			System.out.println(line);
 			Socket socket2 = new Socket(BroadcastServer.stack[1], 50113);
 			PrintStream dataOut2 = new PrintStream(socket2.getOutputStream());
 			dataOut2.println(line);
@@ -151,6 +190,31 @@ public class ServSentDataClient extends Thread {
 		}
 
 	}
+
+	public void sentData(String word, String string,int i) {
+		try {
+			Socket socket1 = new Socket(BroadcastServer.stack[0], 50113);
+			PrintStream dataOut = new PrintStream(socket1.getOutputStream());
+			dataOut.println(word+"-"+string+"-"+i);
+			dataOut.close();
+
+			Socket socket2 = new Socket(BroadcastServer.stack[1], 50113);
+			PrintStream dataOut2 = new PrintStream(socket2.getOutputStream());
+			dataOut2.println(word+"-"+string+"-"+i);
+			dataOut2.close();
+
+			Socket socket3 = new Socket(BroadcastServer.stack[2], 50113);
+			PrintStream dataOut3 = new PrintStream(socket3.getOutputStream());
+			dataOut3.println(word+"-"+string+"-"+i);
+			dataOut3.close();
+
+//			Socket socket4 = new Socket(BroadcastServer.stack[3],50113);
+//			PrintStream dataOut4 = new PrintStream(socket4.getOutputStream());
+//			dataOut4.print(line);
+//			dataOut4.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
 }
-
-
